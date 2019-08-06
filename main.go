30db59c5cf87f3880d822e5953b371398a2ea191
www <18811782570@163.com>
2019-08-06 21:24:14 +0800

    main

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 
+	//"test/project/db"
 	"github.com/LazyboyChen7/project/agent"
 	"github.com/LazyboyChen7/project/transaction"
 )
@@ -23,12 +24,22 @@ func main() {
 	log.Println("The wallet private key  is ", ag.Wlt.PrivateKey.D)
 	log.Println("The wallet public key x is ", ag.Wlt.PrivateKey.PublicKey.X)
 	log.Println("The wallet public key y is ", ag.Wlt.PrivateKey.PublicKey.Y)
+	log.Println("The wallet address is ", string(ag.Wlt.Addr))
 	log.Println("The server port is ", ag.Svr.Port)
 	log.Println("The blockchain height is ", ag.Blc.Bc.Height)
 	log.Println("The blockchain top hash is ", ag.Blc.Bc.TopHash)
 
 	go ag.Start()
 
-	tx := transaction.CreateTx("a", "a", "a")
+	tx := transaction.CreateTx("abc", "abc", "abc")
 	fmt.Println(tx.Thash)
+
+	transaction.Nowpool = transaction.NewTxPool() //初始化交易池
+	transaction.Send("abc", "bcd", "good")        //发送交易1
+	transaction.Send("abc", "bcd", "good1")       //发送交易2
+	transaction.ListTxPool(transaction.Nowpool)   //列出交易
+
+	//db.Putdata()  //存数据
+	//db.Listdata() //列数据
+
 }
